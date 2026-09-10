cb3f5d093e4d508c17dc1d13218fbce98f3ca51f
nst1610 <101926864+nst1610@users.noreply.github.com>
2026-09-11 00:27:12 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/practicum/oauth0/rs/ResourceApp.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.oauth0.rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceApp.class, args);
+    }
+}
